f3f05cf208682dd649987f67a1c1ca91188a7931
wanghaomin <wanghaomin@loongson.cn>
2023-05-08 15:27:47 +0800

    30882: LA port of 8304265: Implementation of Foreign Function and Memory API (Third Preview)
    Reviewed-by: aoqi, panxuefeng

--- src/java.base/share/classes/jdk/internal/foreign/abi/loongarch64/linux/LinuxLoongArch64Linker.java
+++ src/java.base/share/classes/jdk/internal/foreign/abi/loongarch64/linux/LinuxLoongArch64Linker.java
@@ -29,13 +29,9 @@ package jdk.internal.foreign.abi.loongarch64.linux;
 import jdk.internal.foreign.abi.AbstractLinker;
 import jdk.internal.foreign.abi.LinkerOptions;
 
-import java.lang.foreign.SegmentScope;
 import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.MemorySegment;
-import java.lang.foreign.VaList;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
-import java.util.function.Consumer;
 
 public final class LinuxLoongArch64Linker extends AbstractLinker {
 
@@ -57,21 +53,7 @@ public final class LinuxLoongArch64Linker extends AbstractLinker {
     }
 
     @Override
-    protected UpcallStubFactory arrangeUpcall(MethodType targetType, FunctionDescriptor function) {
-        return LinuxLoongArch64CallArranger.arrangeUpcall(targetType, function);
-    }
-
-    public static VaList newVaList(Consumer<VaList.Builder> actions, SegmentScope scope) {
-        LinuxLoongArch64VaList.Builder builder = new LinuxLoongArch64VaList.Builder(scope);
-        actions.accept(builder);
-        return builder.build();
-    }
-
-    public static VaList newVaListOfAddress(long address, SegmentScope scope) {
-        return LinuxLoongArch64VaList.ofAddress(address, scope);
-    }
-
-    public static VaList emptyVaList() {
-        return LinuxLoongArch64VaList.empty();
+    protected UpcallStubFactory arrangeUpcall(MethodType targetType, FunctionDescriptor function, LinkerOptions options) {
+        return LinuxLoongArch64CallArranger.arrangeUpcall(targetType, function, options);
     }
 }

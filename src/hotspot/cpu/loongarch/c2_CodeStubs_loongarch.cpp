8b49ff10239579d33a0cdb3593b2a5a1e9507fa3
wanghaomin <wanghaomin@loongson.cn>
2024-11-28 10:46:55 +0800

    35412: LA port of 8341451: Remove C2HandleAnonOMOwnerStub
    Reviewed-by: aoqi, sunxu, zhaixiang

--- src/hotspot/cpu/loongarch/c2_CodeStubs_loongarch.cpp
+++ src/hotspot/cpu/loongarch/c2_CodeStubs_loongarch.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021, 2023, Loongson Technology. All rights reserved.
+ * Copyright (c) 2021, 2025, Loongson Technology. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -60,32 +60,6 @@ void C2EntryBarrierStub::emit(C2_MacroAssembler& masm) {
   __ bind(guard());
   __ relocate(entry_guard_Relocation::spec());
   __ emit_int32(0);  // nmethod guard value
-}
-
-int C2HandleAnonOMOwnerStub::max_size() const {
-  // Max size of stub has been determined by testing with 0, in which case
-  // C2CodeStubList::emit() will throw an assertion and report the actual size that
-  // is needed.
-  return 24;
-}
-
-void C2HandleAnonOMOwnerStub::emit(C2_MacroAssembler& masm) {
-  __ bind(entry());
-  Register mon = monitor();
-  Register t = tmp();
-  assert(t != noreg, "need tmp register");
-  // Fix owner to be the current thread.
-  __ st_d(TREG, Address(mon, ObjectMonitor::owner_offset()));
-
-  // Pop owner object from lock-stack.
-  __ ld_wu(t, Address(TREG, JavaThread::lock_stack_top_offset()));
-  __ addi_w(t, t, -oopSize);
-#ifdef ASSERT
-  __ stx_d(R0, TREG, t);
-#endif
-  __ st_w(t, Address(TREG, JavaThread::lock_stack_top_offset()));
-
-  __ b(continuation());
 }
 
 #undef __

loongson/jdk21u

commit 526ad75a343fb66f602be42e0d0094f757cac844

Initial Linux/LoongArch64 Port
A complete (i.e., template interpreter, C1 JIT compiler, C2 JIT compiler, Vector API, ZGC and Shenandoah GC) LoongArch64 port has been implemented.

Co-authored-by: Ao Qi <aoqi@loongson.cn>
Co-authored-by: Wang Rui <wangrui@loongson.cn>
Co-authored-by: Zhai Xiang <zhaixiang@loongson.cn>
Co-authored-by: Wang Haomin <wanghaomin@loongson.cn>
Co-authored-by: Sun Guoyun <sunguoyun@loongson.cn>
Co-authored-by: Pan Xuefeng <panxuefeng@loongson.cn>
Co-authored-by: Sun Xu <sunxu01@loongson.cn>

src/hotspot/cpu/loongarch/c1_LinearScan_loongarch.hpp
@@ -1,0 +1,70 @@
+/*
+ * Copyright (c) 2005, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Loongson Technology. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef CPU_LOONGARCH_C1_LINEARSCAN_LOONGARCH_HPP
+#define CPU_LOONGARCH_C1_LINEARSCAN_LOONGARCH_HPP
+
+inline bool LinearScan::is_processed_reg_num(int reg_num) {
+  return reg_num <= FrameMap::last_cpu_reg() || reg_num >= pd_nof_cpu_regs_frame_map;
+}
+
+inline int LinearScan::num_physical_regs(BasicType type) {
+  return 1;
+}
+
+inline bool LinearScan::requires_adjacent_regs(BasicType type) {
+  return false;
+}
+
+inline bool LinearScan::is_caller_save(int assigned_reg) {
+  assert(assigned_reg >= 0 && assigned_reg < nof_regs, "should call this only for registers");
+  if (assigned_reg < pd_first_callee_saved_reg)
+    return true;
+  if (assigned_reg > pd_last_callee_saved_reg && assigned_reg < pd_first_callee_saved_fpu_reg)
+    return true;
+  if (assigned_reg > pd_last_callee_saved_fpu_reg && assigned_reg < pd_last_fpu_reg)
+    return true;
+  return false;
+}
+
+inline void LinearScan::pd_add_temps(LIR_Op* op) {}
+
+// Implementation of LinearScanWalker
+inline bool LinearScanWalker::pd_init_regs_for_alloc(Interval* cur) {
+  if (allocator()->gen()->is_vreg_flag_set(cur->reg_num(), LIRGenerator::callee_saved)) {
+    assert(cur->type() != T_FLOAT && cur->type() != T_DOUBLE, "cpu regs only");
+    _first_reg = pd_first_callee_saved_reg;
+    _last_reg = pd_last_callee_saved_reg;
+    return true;
+  } else if (cur->type() == T_INT || cur->type() == T_LONG || cur->type() == T_OBJECT ||
+             cur->type() == T_ADDRESS || cur->type() == T_METADATA) {
+    _first_reg = pd_first_cpu_reg;
+    _last_reg = pd_last_allocatable_cpu_reg;
+    return true;
+  }
+  return false;
+}
+
+#endif // CPU_LOONGARCH_C1_LINEARSCAN_LOONGARCH_HPP

loongson/jdk

commit 06404c9a458023e9460802d3331e52e892177468

LoongArch: Remove useless caller-saved float registers check
It never successes, because pd_last_callee_saved_fpu_reg is the same as
pd_last_fpu_reg.
LoongArch ABI specified $f24-$f31 as callee-saved registers, while $f31
is also the last register. There is no hole between the last callee-saved
float register and the last float register, so the check always
evaluates to false.

Thanks to clang for finding this.

Fixes: 526ad75a343f ("Initial Linux/LoongArch64 Port")
Signed-off-by: Bingwu Zhang <xtex@aosc.io>

src/hotspot/cpu/loongarch/c1_LinearScan_loongarch.hpp
@@ -44,8 +44,6 @@ inline bool LinearScan::is_caller_save(int assigned_reg) {
     return true;
   if (assigned_reg > pd_last_callee_saved_reg && assigned_reg < pd_first_callee_saved_fpu_reg)
     return true;
-  if (assigned_reg > pd_last_callee_saved_fpu_reg && assigned_reg < pd_last_fpu_reg)
-    return true;
   return false;
 }
 
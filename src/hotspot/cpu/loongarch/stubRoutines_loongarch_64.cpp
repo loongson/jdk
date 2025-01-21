loongson/jdk

commit 09ae79f2af713b50952d014c220a193a57d172dd

Update (2023.12.08)
29494: Fix assert(_succ != current) failed: invariant
32796: Rethinking the ties-to-positive round mode
32874: Amend 30985: Insert acqure membar for load-exclusive with acquire to fix typo
32611: Remove unnecessary initialize constant pool for native wrapper
24942: 8316563: test tools/jpackage/linux/LinuxResourceTest.java fails on CentOS Linux release 8.5.2111 and Fedora 27
32758: Multiple uses of register t7

src/hotspot/cpu/loongarch/stubRoutines_loongarch_64.cpp
@@ -186,3 +186,13 @@ ATTRIBUTE_ALIGNED(64) jdouble StubRoutines::la::_pio2[] = {
   2.73370053816464559624e-44, // 0x36E3822280000000
   2.16741683877804819444e-51, // 0x3569F31D00000000
 };
+
+ATTRIBUTE_ALIGNED(64) jfloat StubRoutines::la::_round_float_imm[] = {
+  0.49999997f, // round positive
+  0.5f,        // round negative
+};
+
+ATTRIBUTE_ALIGNED(64) jdouble StubRoutines::la::_round_double_imm[] = {
+  0.49999999999999994d, // round positive
+  0.5d,                 // round negative
+};

src/hotspot/cpu/loongarch/assembler_loongarch.hpp
@@ -954,6 +954,10 @@ class Assembler : public AbstractAssembler  {
     vshuf_h_op         = 0b01110001011110101,
     vshuf_w_op         = 0b01110001011110110,
     vshuf_d_op         = 0b01110001011110111,
+    vslti_b_op         = 0b01110010100001100,
+    vslti_h_op         = 0b01110010100001101,
+    vslti_w_op         = 0b01110010100001110,
+    vslti_d_op         = 0b01110010100001111,
     vslti_bu_op        = 0b01110010100010000,
     vslti_hu_op        = 0b01110010100010001,
     vslti_wu_op        = 0b01110010100010010,
@@ -1130,6 +1134,10 @@ class Assembler : public AbstractAssembler  {
     xvshuf_w_op        = 0b01110101011110110,
     xvshuf_d_op        = 0b01110101011110111,
     xvperm_w_op        = 0b01110101011111010,
+    xvslti_b_op        = 0b01110110100001100,
+    xvslti_h_op        = 0b01110110100001101,
+    xvslti_w_op        = 0b01110110100001110,
+    xvslti_d_op        = 0b01110110100001111,
     xvslti_bu_op       = 0b01110110100010000,
     xvslti_hu_op       = 0b01110110100010001,
     xvslti_wu_op       = 0b01110110100010010,
@@ -1221,6 +1229,14 @@ class Assembler : public AbstractAssembler  {
     unknow_ops14       = 0b11111111111111
   };
 
+  // 13-bit opcode, highest 13 bits: bits[31...19]
+  enum ops13 {
+    vldrepl_d_op       = 0b0011000000010,
+    xvldrepl_d_op      = 0b0011001000010,
+
+    unknow_ops13       = 0b1111111111111
+  };
+
   // 12-bit opcode, highest 12 bits: bits[31...20]
   enum ops12 {
     fmadd_s_op         = 0b000010000001,
@@ -1258,8 +1274,18 @@ class Assembler : public AbstractAssembler  {
     xvbitsel_v_op      = 0b000011010010,
     vshuf_b_op         = 0b000011010101,
     xvshuf_b_op        = 0b000011010110,
+    vldrepl_w_op       = 0b001100000010,
+    xvldrepl_w_op      = 0b001100100010,
 
     unknow_ops12       = 0b111111111111
+  };
+
+  // 11-bit opcode, highest 11 bits: bits[31...21]
+  enum ops11 {
+    vldrepl_h_op       = 0b00110000010,
+    xvldrepl_h_op      = 0b00110010010,
+
+    unknow_ops11       = 0b11111111111
   };
 
   // 10-bit opcode, highest 10 bits: bits[31...22]
@@ -1297,6 +1323,8 @@ class Assembler : public AbstractAssembler  {
     xvst_op            = 0b0010110011,
     ldl_w_op           = 0b0010111000,
     ldr_w_op           = 0b0010111001,
+    vldrepl_b_op       = 0b0011000010,
+    xvldrepl_b_op      = 0b0011001010,
 
     unknow_ops10       = 0b1111111111
   };
@@ -1475,10 +1503,25 @@ class Assembler : public AbstractAssembler  {
   // |   opcode           |    I8   |    rj  |  rd   |
   static inline int insn_I8RR (int op, int imm8, int rj, int rd)  { /*assert(is_simm(imm8, 8), "not a signed 8-bit int");*/ return (op<<18) | (low(imm8, 8)<<10) | (rj<<5) | rd; }
 
+  // 2RI9-type
+  //  31              19 18       10 9      5 4     0
+  // |   opcode         |     I9    |    rj  |  vd   |
+  static inline int insn_I9RR(int op, int imm9, int rj, int vd)  { return (op<<19) | (low(imm9, 9)<<10) | (rj<<5) | vd; }
+
+  // 2RI10-type
+  //  31             20 19        10 9      5 4     0
+  // |   opcode        |    I10     |    rj  |  vd   |
+  static inline int insn_I10RR(int op, int imm10, int rj, int vd) { return (op<<20) | (low(imm10, 10)<<10) | (rj<<5) | vd; }
+
+  // 2RI11-type
+  //  31            21 20         10 9      5 4     0
+  // |   opcode       |     I11     |    rj  |  vd   |
+  static inline int insn_I11RR(int op, int imm11, int rj, int vd) { return (op<<21) | (low(imm11, 11)<<10) | (rj<<5) | vd; }
+
   // 2RI12-type
   //  31           22 21          10 9      5 4     0
   // |   opcode      |     I12      |    rj  |  rd   |
-  static inline int insn_I12RR(int op, int imm12, int rj, int rd) { /* assert(is_simm(imm12, 12), "not a signed 12-bit int");*/  return (op<<22) | (low(imm12, 12)<<10) | (rj<<5) | rd; }
+  static inline int insn_I12RR(int op, int imm12, int rj, int rd) { return (op<<22) | (low(imm12, 12)<<10) | (rj<<5) | rd; }
 
   // 2RI14-type
   //  31         24 23            10 9      5 4     0
@@ -2866,6 +2909,15 @@ public:
   void xvslt_wu(FloatRegister xd, FloatRegister xj, FloatRegister xk) { ASSERT_LASX emit_int32(insn_RRR(xvslt_wu_op, (int)xk->encoding(), (int)xj->encoding(), (int)xd->encoding())); }
   void xvslt_du(FloatRegister xd, FloatRegister xj, FloatRegister xk) { ASSERT_LASX emit_int32(insn_RRR(xvslt_du_op, (int)xk->encoding(), (int)xj->encoding(), (int)xd->encoding())); }
 
+  void  vslti_b(FloatRegister vd, FloatRegister vj, int si5) { ASSERT_LSX  assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR( vslti_b_op, si5, (int)vj->encoding(), (int)vd->encoding())); }
+  void  vslti_h(FloatRegister vd, FloatRegister vj, int si5) { ASSERT_LSX  assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR( vslti_h_op, si5, (int)vj->encoding(), (int)vd->encoding())); }
+  void  vslti_w(FloatRegister vd, FloatRegister vj, int si5) { ASSERT_LSX  assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR( vslti_w_op, si5, (int)vj->encoding(), (int)vd->encoding())); }
+  void  vslti_d(FloatRegister vd, FloatRegister vj, int si5) { ASSERT_LSX  assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR( vslti_d_op, si5, (int)vj->encoding(), (int)vd->encoding())); }
+  void xvslti_b(FloatRegister xd, FloatRegister xj, int si5) { ASSERT_LASX assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR(xvslti_b_op, si5, (int)xj->encoding(), (int)xd->encoding())); }
+  void xvslti_h(FloatRegister xd, FloatRegister xj, int si5) { ASSERT_LASX assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR(xvslti_h_op, si5, (int)xj->encoding(), (int)xd->encoding())); }
+  void xvslti_w(FloatRegister xd, FloatRegister xj, int si5) { ASSERT_LASX assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR(xvslti_w_op, si5, (int)xj->encoding(), (int)xd->encoding())); }
+  void xvslti_d(FloatRegister xd, FloatRegister xj, int si5) { ASSERT_LASX assert(is_simm(si5, 5), "not a signed 5-bit int"); emit_int32(insn_I5RR(xvslti_d_op, si5, (int)xj->encoding(), (int)xd->encoding())); }
+
   void  vslti_bu(FloatRegister vd, FloatRegister vj, int ui5) { ASSERT_LSX  emit_int32(insn_I5RR( vslti_bu_op, ui5, (int)vj->encoding(), (int)vd->encoding())); }
   void  vslti_hu(FloatRegister vd, FloatRegister vj, int ui5) { ASSERT_LSX  emit_int32(insn_I5RR( vslti_hu_op, ui5, (int)vj->encoding(), (int)vd->encoding())); }
   void  vslti_wu(FloatRegister vd, FloatRegister vj, int ui5) { ASSERT_LSX  emit_int32(insn_I5RR( vslti_wu_op, ui5, (int)vj->encoding(), (int)vd->encoding())); }
@@ -3103,6 +3155,15 @@ public:
 
   void  vstx(FloatRegister vd, Register rj, Register rk) { ASSERT_LSX  emit_int32(insn_RRR( vstx_op, (int)rk->encoding(), (int)rj->encoding(), (int)vd->encoding())); }
   void xvstx(FloatRegister xd, Register rj, Register rk) { ASSERT_LASX emit_int32(insn_RRR(xvstx_op, (int)rk->encoding(), (int)rj->encoding(), (int)xd->encoding())); }
+
+  void  vldrepl_d(FloatRegister vd, Register rj, int si9) { ASSERT_LSX  assert(is_simm(si9, 9), "not a signed 9-bit int"); emit_int32(insn_I9RR( vldrepl_d_op, si9, (int)rj->encoding(), (int)vd->encoding()));}
+  void  vldrepl_w(FloatRegister vd, Register rj, int si10) { ASSERT_LSX  assert(is_simm(si10, 10), "not a signed 10-bit int"); emit_int32(insn_I10RR( vldrepl_w_op, si10, (int)rj->encoding(), (int)vd->encoding()));}
+  void  vldrepl_h(FloatRegister vd, Register rj, int si11) { ASSERT_LSX  assert(is_simm(si11, 11), "not a signed 11-bit int"); emit_int32(insn_I11RR( vldrepl_h_op, si11, (int)rj->encoding(), (int)vd->encoding()));}
+  void  vldrepl_b(FloatRegister vd, Register rj, int si12) { ASSERT_LSX  assert(is_simm(si12, 12), "not a signed 12-bit int"); emit_int32(insn_I12RR( vldrepl_b_op, si12, (int)rj->encoding(), (int)vd->encoding()));}
+  void xvldrepl_d(FloatRegister xd, Register rj, int si9) { ASSERT_LASX assert(is_simm(si9, 9), "not a signed 9-bit int"); emit_int32(insn_I9RR(xvldrepl_d_op, si9, (int)rj->encoding(), (int)xd->encoding()));}
+  void xvldrepl_w(FloatRegister xd, Register rj, int si10) { ASSERT_LASX assert(is_simm(si10, 10), "not a signed 10-bit int"); emit_int32(insn_I10RR(xvldrepl_w_op, si10, (int)rj->encoding(), (int)xd->encoding()));}
+  void xvldrepl_h(FloatRegister xd, Register rj, int si11) { ASSERT_LASX assert(is_simm(si11, 11), "not a signed 11-bit int"); emit_int32(insn_I11RR(xvldrepl_h_op, si11, (int)rj->encoding(), (int)xd->encoding()));}
+  void xvldrepl_b(FloatRegister xd, Register rj, int si12) { ASSERT_LASX assert(is_simm(si12, 12), "not a signed 12-bit int"); emit_int32(insn_I12RR(xvldrepl_b_op, si12, (int)rj->encoding(), (int)xd->encoding()));}
 
 #undef ASSERT_LSX
 #undef ASSERT_LASX

src/hotspot/cpu/loongarch/c1_LIRAssembler_loongarch_64.cpp
@@ -1324,7 +1324,9 @@ void LIR_Assembler::emit_typecheck_helper(LIR_OpTypeCheck *op, Label* success,
     __ load_klass(recv, obj);
     type_profile_helper(mdo, md, data, recv, &update_done);
     Address counter_addr(mdo, md->byte_offset_of_slot(data, CounterData::count_offset()));
-    __ increment(counter_addr, DataLayout::counter_increment);
+    __ ld_d(SCR2, counter_addr);
+    __ addi_d(SCR2, SCR2, DataLayout::counter_increment);
+    __ st_d(SCR2, counter_addr);
     __ bind(update_done);
   } else {
     __ beqz(obj, *obj_is_null);
@@ -1436,7 +1438,9 @@ void LIR_Assembler::emit_opTypeCheck(LIR_OpTypeCheck* op) {
       __ load_klass(recv, value);
       type_profile_helper(mdo, md, data, recv, &update_done);
       Address counter_addr(mdo, md->byte_offset_of_slot(data, CounterData::count_offset()));
-      __ increment(counter_addr, DataLayout::counter_increment);
+      __ ld_d(SCR2, counter_addr);
+      __ addi_d(SCR2, SCR2, DataLayout::counter_increment);
+      __ st_d(SCR2, counter_addr);
       __ bind(update_done);
     } else {
       __ beqz(value, done);

src/hotspot/cpu/loongarch/gc/x/x_loongarch_64.ad
@@ -56,12 +56,12 @@ static void x_compare_and_swap(MacroAssembler& _masm, const MachNode* node,
   // Weak CAS operations are thus only emitted if the barrier is elided.
   Address addr(mem);
   if (node->barrier_data() == XLoadBarrierElided) {
-    __ cmpxchg(addr, oldval, newval, tmp, false /* retold */, acquire /* barrier */,
+    __ cmpxchg(addr, oldval, newval, tmp, false /* retold */, acquire /* acquire */,
                weak /* weak */, false /* exchange */);
     __ move(res, tmp);
   } else {
     __ move(tmp, oldval);
-    __ cmpxchg(addr, tmp, newval, AT, true /* retold */, acquire /* barrier */,
+    __ cmpxchg(addr, tmp, newval, AT, true /* retold */, acquire /* acquire */,
                false /* weak */, false /* exchange */);
     __ move(res, AT);
 
@@ -70,7 +70,7 @@ static void x_compare_and_swap(MacroAssembler& _masm, const MachNode* node,
     __ andr(AT, AT, tmp);
     __ beqz(AT, good);
     x_load_barrier_slow_path(_masm, node, addr, tmp, res /* used as tmp */);
-    __ cmpxchg(addr, oldval, newval, tmp, false /* retold */, acquire /* barrier */, weak /* weak */, false /* exchange */);
+    __ cmpxchg(addr, oldval, newval, tmp, false /* retold */, acquire /* acquire */, weak /* weak */, false /* exchange */);
     __ move(res, tmp);
     __ bind(good);
   }

src/hotspot/cpu/loongarch/loongarch_64.ad
@@ -10870,6 +10870,28 @@ instruct convHF2F_reg_reg(regF dst, mRegI src, regF tmp) %{
   ins_pipe(pipe_slow);
 %}
 
+instruct round_float_reg(mRegI dst, regF src, mRegL tmp)
+%{
+  match(Set dst (RoundF src));
+  effect(TEMP_DEF dst, TEMP tmp);
+  format %{ "round_float $dst, $src\t# @round_float_reg" %}
+  ins_encode %{
+    __ java_round_float($dst$$Register, $src$$FloatRegister, $tmp$$Register);
+  %}
+  ins_pipe( pipe_slow );
+%}
+
+instruct round_double_reg(mRegL dst, regD src, mRegL tmp)
+%{
+  match(Set dst (RoundD src));
+  effect(TEMP_DEF dst, TEMP tmp);
+  format %{ "round_double $dst, $src\t# @round_double_reg" %}
+  ins_encode %{
+    __ java_round_double($dst$$Register, $src$$FloatRegister, $tmp$$Register);
+  %}
+  ins_pipe( pipe_slow );
+%}
+
 instruct roundD(regD dst, regD src, immI rmode) %{
   predicate(UseLSX);
   match(Set dst (RoundDoubleMode src rmode));
@@ -12512,26 +12534,6 @@ instruct safePoint_poll_tls(mRegP poll) %{
   %}
 
   ins_pipe( pipe_serial );
-%}
-
-//----------Arithmetic Conversion Instructions---------------------------------
-
-instruct roundFloat_nop(regF dst)
-%{
-  match(Set dst (RoundFloat dst));
-
-  ins_cost(0);
-  ins_encode();
-  ins_pipe( empty );
-%}
-
-instruct roundDouble_nop(regD dst)
-%{
-  match(Set dst (RoundDouble dst));
-
-  ins_cost(0);
-  ins_encode();
-  ins_pipe( empty );
 %}
 
 //----------BSWAP Instructions-------------------------------------------------
@@ -14219,6 +14221,64 @@ instruct reduceVD(regD dst, regD src, vReg vsrc, vReg tmp) %{
   ins_encode %{
     __ reduce($dst$$FloatRegister, $src$$FloatRegister, $vsrc$$FloatRegister, $tmp$$FloatRegister,
               T_DOUBLE, this->ideal_Opcode(), Matcher::vector_length_in_bytes(this, $vsrc));
+  %}
+  ins_pipe( pipe_slow );
+%}
+
+// ------------------------------ Vector Round ---------------------------------
+
+instruct round_float_lsx(vReg dst, vReg src, vReg vtemp1, vReg vtemp2) %{
+  predicate(Matcher::vector_length_in_bytes(n) <= 16);
+  match(Set dst (RoundVF src));
+  effect(TEMP_DEF dst, TEMP vtemp1, TEMP vtemp2);
+  format %{ "round_float_lsx $dst, $src\t# @round_float_lsx" %}
+  ins_encode %{
+    __ java_round_float_lsx($dst$$FloatRegister,
+                            $src$$FloatRegister,
+                            $vtemp1$$FloatRegister,
+                            $vtemp2$$FloatRegister);
+  %}
+  ins_pipe( pipe_slow );
+%}
+
+instruct round_float_lasx(vReg dst, vReg src, vReg vtemp1, vReg vtemp2) %{
+  predicate(Matcher::vector_length_in_bytes(n) > 16);
+  match(Set dst (RoundVF src));
+  effect(TEMP_DEF dst, TEMP vtemp1, TEMP vtemp2);
+  format %{ "round_float_lasx $dst, $src\t# @round_float_lasx" %}
+  ins_encode %{
+    __ java_round_float_lasx($dst$$FloatRegister,
+                             $src$$FloatRegister,
+                             $vtemp1$$FloatRegister,
+                             $vtemp2$$FloatRegister);
+  %}
+  ins_pipe( pipe_slow );
+%}
+
+instruct round_double_lsx(vReg dst, vReg src, vReg vtemp1, vReg vtemp2) %{
+  predicate(Matcher::vector_length_in_bytes(n) <= 16);
+  match(Set dst (RoundVD src));
+  effect(TEMP_DEF dst, TEMP vtemp1, TEMP vtemp2);
+  format %{ "round_double_lsx $dst, $src\t# @round_double_lsx" %}
+  ins_encode %{
+    __ java_round_double_lsx($dst$$FloatRegister,
+                             $src$$FloatRegister,
+                             $vtemp1$$FloatRegister,
+                             $vtemp2$$FloatRegister);
+  %}
+  ins_pipe( pipe_slow );
+%}
+
+instruct round_double_lasx(vReg dst, vReg src, vReg vtemp1, vReg vtemp2) %{
+  predicate(Matcher::vector_length_in_bytes(n) > 16);
+  match(Set dst (RoundVD src));
+  effect(TEMP_DEF dst, TEMP vtemp1, TEMP vtemp2);
+  format %{ "round_double_lasx $dst, $src\t# @round_double_lasx" %}
+  ins_encode %{
+    __ java_round_double_lasx($dst$$FloatRegister,
+                              $src$$FloatRegister,
+                              $vtemp1$$FloatRegister,
+                              $vtemp2$$FloatRegister);
   %}
   ins_pipe( pipe_slow );
 %}

src/hotspot/cpu/loongarch/macroAssembler_loongarch.cpp
@@ -3430,6 +3430,144 @@ void MacroAssembler::encode_iso_array(Register src, Register dst,
   bind(Done);
 }
 
+// Math.round employs the ties-to-positive round mode,
+// which is not a typically conversion method defined
+// in the IEEE-754-2008. For single-precision floatings,
+// the following algorithm can be used to effectively
+// implement rounding via standard operations.
+//
+// if src >= 0:
+//   dst = floor(src + 0.49999997f)
+// else:
+//   dst = floor(src + 0.5f)
+void MacroAssembler::java_round_float(Register dst,
+                                      FloatRegister src,
+                                      Register tmp) {
+  block_comment("java_round_float: { ");
+  li(AT, StubRoutines::la::round_float_imm());
+
+  movfr2gr_s(tmp, src);
+  bstrpick_w(tmp, tmp, 31, 31);
+  slli_w(tmp, tmp, 2);
+  fldx_s(fscratch, AT, tmp);
+  fadd_s(fscratch, fscratch, src);
+
+  ftintrm_w_s(fscratch, fscratch);
+  movfr2gr_s(dst, fscratch);
+  block_comment("} java_round_float");
+}
+
+void MacroAssembler::java_round_float_lsx(FloatRegister dst,
+                                          FloatRegister src,
+                                          FloatRegister vtemp1,
+                                          FloatRegister vtemp2) {
+  block_comment("java_round_float_lsx: { ");
+  li(AT, StubRoutines::la::round_float_imm());
+
+  vldrepl_w(vtemp2, AT, 1);  // repl 0.5f
+  vslti_w(fscratch, src, 0);  // masked add
+  vand_v(vtemp2, fscratch, vtemp2);
+  vfadd_s(dst, src, vtemp2);
+
+  vldrepl_w(vtemp1, AT, 0);  // repl 0.49999997f
+  vnor_v(fscratch, fscratch, fscratch);  // rev mask
+  vand_v(vtemp1, fscratch, vtemp1);
+  vfadd_s(dst, dst, vtemp1);
+
+  vftintrm_w_s(dst, dst);
+  block_comment("} java_round_float_lsx");
+}
+
+void MacroAssembler::java_round_float_lasx(FloatRegister dst,
+                                           FloatRegister src,
+                                           FloatRegister vtemp1,
+                                           FloatRegister vtemp2) {
+  block_comment("java_round_float_lasx: { ");
+  li(AT, StubRoutines::la::round_float_imm());
+
+  xvldrepl_w(vtemp2, AT, 1);  // repl 0.5f
+  xvslti_w(fscratch, src, 0);  // masked add
+  xvand_v(vtemp2, fscratch, vtemp2);
+  xvfadd_s(dst, src, vtemp2);
+
+  xvldrepl_w(vtemp1, AT, 0);  // repl 0.49999997f
+  xvnor_v(fscratch, fscratch, fscratch);  // rev mask
+  xvand_v(vtemp1, fscratch, vtemp1);
+  xvfadd_s(dst, dst, vtemp1);
+
+  xvftintrm_w_s(dst, dst);
+  block_comment("} java_round_float_lasx");
+}
+
+// Math.round employs the ties-to-positive round mode,
+// which is not a typically conversion method defined
+// in the IEEE-754-2008. For double-precision floatings,
+// the following algorithm can be used to effectively
+// implement rounding via standard operations.
+//
+// if src >= 0:
+//   dst = floor(src + 0.49999999999999994d)
+// else:
+//   dst = floor(src + 0.5d)
+void MacroAssembler::java_round_double(Register dst,
+                                       FloatRegister src,
+                                       Register tmp) {
+  block_comment("java_round_double: { ");
+  li(AT, StubRoutines::la::round_double_imm());
+
+  movfr2gr_d(tmp, src);
+  bstrpick_d(tmp, tmp, 63, 63);
+  slli_d(tmp, tmp, 3);
+  fldx_d(fscratch, AT, tmp);
+  fadd_d(fscratch, fscratch, src);
+
+  ftintrm_l_d(fscratch, fscratch);
+  movfr2gr_d(dst, fscratch);
+  block_comment("} java_round_double");
+}
+
+void MacroAssembler::java_round_double_lsx(FloatRegister dst,
+                                           FloatRegister src,
+                                           FloatRegister vtemp1,
+                                           FloatRegister vtemp2) {
+  block_comment("java_round_double_lsx: { ");
+  li(AT, StubRoutines::la::round_double_imm());
+
+  vldrepl_d(vtemp2, AT, 1);  // repl 0.5d
+  vslti_d(fscratch, src, 0);  // masked add
+  vand_v(vtemp2, fscratch, vtemp2);
+  vfadd_d(dst, src, vtemp2);
+
+  vldrepl_d(vtemp1, AT, 0);  // repl 0.49999999999999994d
+  vnor_v(fscratch, fscratch, fscratch);  // rev mask
+  vand_v(vtemp1, fscratch, vtemp1);
+  vfadd_d(dst, dst, vtemp1);
+
+  vftintrm_l_d(dst, dst);
+  block_comment("} java_round_double_lsx");
+}
+
+void MacroAssembler::java_round_double_lasx(FloatRegister dst,
+                                            FloatRegister src,
+                                            FloatRegister vtemp1,
+                                            FloatRegister vtemp2) {
+  block_comment("java_round_double_lasx: { ");
+  li(AT, StubRoutines::la::round_double_imm());
+
+  xvldrepl_d(vtemp2, AT, 1);  // repl 0.5d
+  xvslti_d(fscratch, src, 0);  // masked add
+  xvand_v(vtemp2, fscratch, vtemp2);
+  xvfadd_d(dst, src, vtemp2);
+
+  xvldrepl_d(vtemp1, AT, 0);  // repl 0.49999999999999994d
+  xvnor_v(fscratch, fscratch, fscratch);  // rev mask
+  xvand_v(vtemp1, fscratch, vtemp1);
+  xvfadd_d(dst, dst, vtemp1);
+
+  xvftintrm_l_d(dst, dst);
+  block_comment("} java_round_double_lasx");
+}
+
 // Code for BigInteger::mulAdd intrinsic
 // out     = c_rarg0
 // in      = c_rarg1

src/hotspot/cpu/loongarch/macroAssembler_loongarch.hpp
@@ -315,6 +315,18 @@ class MacroAssembler: public Assembler {
   void sign_extend_short(Register reg) { ext_w_h(reg, reg); }
   void sign_extend_byte(Register reg)  { ext_w_b(reg, reg); }
 
+  // java.lang.Math::round intrinsics
+  void java_round_float(Register dst, FloatRegister src, Register tmp);
+  void java_round_float_lsx(FloatRegister dst, FloatRegister src,
+                            FloatRegister vtemp1, FloatRegister vtemp2);
+  void java_round_float_lasx(FloatRegister dst, FloatRegister src,
+                             FloatRegister vtemp1, FloatRegister vtemp2);
+  void java_round_double(Register dst, FloatRegister src, Register tmp);
+  void java_round_double_lsx(FloatRegister dst, FloatRegister src,
+                             FloatRegister vtemp1, FloatRegister vtemp2);
+  void java_round_double_lasx(FloatRegister dst, FloatRegister src,
+                              FloatRegister vtemp1, FloatRegister vtemp2);
+
   // allocation
   void tlab_allocate(
     Register obj,                      // result: pointer to object after successful allocation

src/hotspot/cpu/loongarch/stubRoutines_loongarch.hpp
@@ -63,6 +63,9 @@ class la {
 
   static julong _string_compress_index[];
 
+  static jfloat _round_float_imm[];
+  static jdouble _round_double_imm[];
+
   // begin trigonometric tables block. See comments in .cpp file
   static juint    _npio2_hw[];
   static jdouble   _two_over_pi[];
@@ -101,6 +104,14 @@ public:
 
   static address string_compress_index() {
     return (address) _string_compress_index;
+  }
+
+  static address round_float_imm() {
+    return (address) _round_float_imm;
+  }
+
+  static address round_double_imm() {
+    return (address) _round_double_imm;
   }
 };
 

commit 290544200e0f3d1adc31a58a237015b1675c2c1e

Remove invalid 'd' suffix for double literals
Per C++ 14 specification, "2.14.4 Floating literals", 'd' is not a
standardized floating-suffix.
The 'd' floating-suffix is a GCC extension, which worked on GCC by
default but not supported by Clang and many other C compilers.

Having no suffix makes a constant of double, which is expected here, so
it can be removed safely.

Fixes: 09ae79f2af71 ("Update (2023.12.08)")
Signed-off-by: Bingwu Zhang <xtex@aosc.io>

src/hotspot/cpu/loongarch/stubRoutines_loongarch_64.cpp
@@ -191,5 +191,5 @@ ATTRIBUTE_ALIGNED(64) jfloat StubRoutines::la::_round_float_imm[] = {
 };
 
 ATTRIBUTE_ALIGNED(64) jdouble StubRoutines::la::_round_double_imm[] = {
-  -0.5d, 0.49999999999999994d // magic number for ties
+  -0.5, 0.49999999999999994 // magic number for ties
 };